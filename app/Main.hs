00b99fae188f5eb5868b0363dd50907bef185eac
Dan Sparks <lordaniel24@gmail.com>
2023-03-03 16:43:45 -0600

    typeclasses

--- app/Main.hs
+++ app/Main.hs
@@ -74,5 +74,32 @@ encode (x:xs) = let (first, rest) = span (==x) xs
                     in (length (x:first), x:first) : encode rest
 encode [] = []
 
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton' :: a -> Tree a
+singleton' x = Node x EmptyTree EmptyTree
+
+treeInsert :: Ord a => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton' x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+-- problem 11
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "red gem"  
+    show Green = "green gem"
+    show Yellow = "yellow gem"    
+
+
 main :: IO ()
 main = putStrLn "Goodbye cruel world..."
